493f6df1d49bc42f9aca64be99e638007f365fb3
Diego Avellaneda <daacdiego2015@gmail.com>
2026-01-30 14:22:01 -0500

    Creamos carpeta de back

--- frontend/src/Inicio/Inicio.js
+++ frontend/src/Inicio/Inicio.js
@@ -4,9 +4,15 @@ import { useState } from "react";
 import municipiosData from "../municipiosCUn.json";
 
 
+
 function Inicio() {
     const [dataSearch, setDataSearch] = useState("");
+    const [nombre, setNombre] = useState("");
+    const [celular, setCelular] = useState("");
+    const [municipio, setMunicipio] = useState("");
+    const [mensaje, setMensaje] = useState("");
     const municipios = municipiosData;
+
 
     return (
         <>
@@ -31,21 +37,84 @@ function Inicio() {
                 <div className="formulario-contacto">
                     <form>
                         <p className="titulo-formulario">Contáctanos</p>
-                        <input id="nombreForm" type="text" placeholder="Nombre" />
+                        <input
+                            id="nombreForm"
+                            type="text"
+                            placeholder="Nombre"
+                            value={nombre}
+                            onChange={
+                                (event) => {
+                                    setNombre(event.target.value);
+                                }
+                            }
+                            required
+                        />
                         <div className="celular-municipio">
-                            <input id="celularForm" type="phone" placeholder="Celular" />
-                            <input id="municipioForm" list="municipio" placeholder="Municipio" />
+                            <input
+                                id="celularForm"
+                                type="number"
+                                placeholder="Celular"
+                                value={celular}
+                                onChange={
+                                    (event) => {
+                                        setCelular(event.target.value);
+                                    }
+                                }
+                                required
+                            />
+                            <input
+                                id="municipioForm"
+                                list="municipio"
+                                placeholder="Municipio"
+                                value={municipio}
+                                onChange={
+                                    (event) => {
+                                        setMunicipio(event.target.value);
+                                    }
+                                }
+                                required
+                            />
                             <datalist id="municipio">
                                 {
                                     municipios.map(nombreMunicipio => (
-                                        <option>{nombreMunicipio}</option>
+                                        <option key={nombreMunicipio}>{nombreMunicipio}</option>
                                     ))
                                 }
                             </datalist>
                         </div>
-                        <input id="mensaje" type="text" name="mensaje" maxlength="100" placeholder="Mensaje" />
-                        <button>Enviar</button>
+                        <input
+                            id="mensaje"
+                            type="text"
+                            name="mensaje"
+                            placeholder="Mensaje"
+                            value={mensaje}
+                            onChange={
+                                (event) => {
+                                    setMensaje(event.target.value);
+                                }
+                            }
+                            required
+                        />
+                        <div className="btnFormularioContacto">
+                            <button
+                                onClick={
+                                    (event) => {
+                                        event.preventDefault();
+                                        const whatsApptelefono = 3103105286;
+                                        const datoMensaje = "Hola, soy " + nombre + " del municipio de " + municipio + " y tengo este caso: " + mensaje;
+                                        const enviarMensaje = "https://wa.me/" + whatsApptelefono + "?text=" + datoMensaje;
+                                        window.open(enviarMensaje, "_blanck");
+                                        setNombre("");
+                                        setCelular("");
+                                        setMunicipio("");
+                                        setMensaje("");
+                                    }
+                                }>Enviar</button>
+                        </div>
                     </form>
+                </div>
+                <div className="SectionYoutube">
+                    
                 </div>
             </section>
         </>
